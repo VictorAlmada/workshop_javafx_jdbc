59b9b9e0e452fb90df49ba8d87c1de7be330e300
Victor Almada <victor_mathers@hotmail.com>
2024-11-22 19:07:12 -0300

    Saving a new department

--- src/module-info.java
+++ src/module-info.java
@@ -3,11 +3,20 @@ module workshop_javafx_jdbc {
 	requires javafx.fxml;
 	requires lombok;
 	requires java.sql;
+	requires transitive javafx.base;
+	requires transitive javafx.graphics;
+
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.fxml;
 	opens model.entities to javafx.base;
 
     exports gui;
-	
+	exports application;
+	exports db;
+	exports gui.util;
+	exports model.dao;
+	exports model.dao.impl;
+	exports model.entities;
+	exports model.services;
 }
